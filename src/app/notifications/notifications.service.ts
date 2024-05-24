64a4c6a0a3adc5e8c831f1b9de9c3e6445324330
Afan Dayu Laksono <48554914+fundayluck@users.noreply.github.com>
2024-05-24 11:31:48 +0700

    more design on notifications

--- src/app/notifications/notifications.service.ts
+++ src/app/notifications/notifications.service.ts
@@ -1,9 +1,23 @@
 import { Injectable } from '@angular/core';
+import { Subject } from 'rxjs';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class NotificationsService {
+  messages: Subject<any>;
 
-  constructor() { }
+  constructor() {
+    this.messages = new Subject<any>();
+  }
+
+  addSuccess(messages: string) {
+    this.messages.next(messages);
+  }
+
+  addError(messages: string) {
+    this.messages.next(messages);
+  }
+
+  clearMessage(id: number) {}
 }
