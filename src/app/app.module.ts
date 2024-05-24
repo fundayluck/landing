c7326f79b20290d9a13ac3dc56ce6f79ad21a6b4
Afan Dayu Laksono <48554914+fundayluck@users.noreply.github.com>
2024-05-24 10:51:46 +0700

    generating the notification module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { AppComponent } from './app.component';
 import { BrowserModule } from '@angular/platform-browser';
 import { WeatherModule } from './weather/weather.module';
 import { HttpClientModule } from '@angular/common/http';
+import { NotificationsModule } from './notifications/notifications.module';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, HttpClientModule, WeatherModule],
+  imports: [
+    BrowserModule,
+    HttpClientModule,
+    WeatherModule,
+    NotificationsModule,
+  ],
   providers: [],
   bootstrap: [AppComponent],
 })
---
+++ src/app/notifications/notification-list/notification-list.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-notification-list',
+  templateUrl: './notification-list.component.html',
+  styleUrl: './notification-list.component.css',
+})
+export class NotificationListComponent {}
